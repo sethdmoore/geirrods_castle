d73113872d870f491a6cead1dfcf218a912a8235
Seth Moore <mcapplbee@gmail.com>
2017-05-09 03:22:49 -0400

    Move a lot of logic for choices and parsing into util

--- main.py
+++ main.py
@@ -5,35 +5,47 @@ import story
 from story import basic_campaign
 
 
+def introduction(playable_characters):
+    print("Welcome to Geirrod's Castle.")
+    name = util.Get_Name()
 
-def introduction():
-    print("Welcome to Geirrod's Castle")
-    print("Please choose your class")
+    print("Please choose your class.")
+
+    player_class = event_loop(playable_characters, confirm=True)
+    return name, player_class
 
 
-def pester(choices):
-    print("Valid choices are %s" % ", ".join((choices)))
+def event_loop(choices, confirm=False):
+    # pester(choices)
+    output = ", ".join(choices)
+    choice = util.Prompt(text=output, confirm=confirm)
 
-
-def event_loop(choices):
-    pester(choices)
-    choice = util.Prompt()
     while choice not in choices:
-        pester(choices)
-        choice = util.Prompt()
+        # pester(choices)
+        choice = util.Prompt(text=output, confirm=confirm)
+
+    return choice
 
 
 def main():
-    introduction()
     # probably should name space that better, but it does show you the
     # module / namespace hierarchy in python
     playable = characters.classes.show_playable_characters()
 
-    event_loop(playable)
+    name, player_class = introduction(playable)
+
+
+    player = characters.New(player_class, name)
+
+    # in theory if you wanted to make your own campaign you would create
+    # story/foo_campaign.py and add a scenario dictionary there.
+    # Or you could be crazy awesome and make it read from some fancy JSON-spewing
+    # API in the fluffy clouds somewhere.
     campaign = story.Campaign(basic_campaign.scenario)
 
-    player = characters.New()
     campaign.Chapter("preface")
+
+    chocie = event_loop(campaign.choices)
 
 
 if __name__ == '__main__':
--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,56 @@
-# the token used to show player input
-token = "> "
+def To_Bool(arg):
+    a = arg.lower()
+    if a == "y" or a == "yes" or a == "true" or a == "affirmative":
+        return True
+    elif a == "n" or a == "no" or a == "false" or a == "negative":
+        return False
+    else:
+        return None
 
-def Prompt():
-    action = input(token)
+
+def print_text(text, end="\n"):
+    if text is not None:
+        print(text, end=end)
+
+
+def Prompt(text=None, confirm=False):
+    # the token used to show player input
+    end="\n"
+    token = "> "
+    confirmed = False
+
+    # ask the user to confirm their choice
+    if confirm:
+        while not confirmed:
+            print_text(text)
+            action = input(token)
+            print("\"%s\"? Is this correct? " % action, end="")
+            confirmed = Prompt_Bool()
+    # simple dialog
+    else:
+        print_text(text, end=end)
+        action = input(token)
+
     return action
+
+
+def Prompt_Bool():
+    result = None
+    while result is None:
+        result = To_Bool(Prompt(text="[Y/n]"))
+    return result
+
+
+def Get_Name():
+    name = None
+    while name is None:
+        name = Prompt(text="Please tell me your name.", confirm=True)
+        # basic input validation
+        if len(name) == 0:
+            name = None
+            continue
+        if name[0] == " ":
+            name = None
+            continue
+
+    return name
